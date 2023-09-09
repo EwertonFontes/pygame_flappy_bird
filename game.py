6fbf67de02d8f888fdf30e3b932e31ca899d04d6
Ewerton Fontes <ewerton.p.fontes@gmail.com>
2023-09-09 17:38:53 -0300

    moving and animated backgrounds

--- game.py
+++ game.py
@@ -7,15 +7,37 @@ class GameScene:
     def __init__(self):
         self.all_sprites = pygame.sprite.Group()
         self.backgroud_image = ScreenElements("assets/sky.png", 0, 0, self.all_sprites)
+        self.backgroud_image_aux = ScreenElements("assets/sky.png", 360, 0, self.all_sprites)
         self.ground = ScreenElements("assets/ground.png", 0, 480, self.all_sprites)
+        self.ground_aux = ScreenElements("assets/ground.png", 360, 480, self.all_sprites)
         self.change_scene = False
     
     def draw(self, window):
         self.all_sprites.draw(window) 
 
     def update(self):
+        self.move_bg()
+        self.move_groud()
         self.all_sprites.update()
 
     
+    def move_bg(self):
+        self.backgroud_image.rect[0] -= 1
+        self.backgroud_image_aux.rect[0] -= 1
+
+        if self.backgroud_image.rect[0] <= -360:
+            self.backgroud_image.rect[0] = 0
+        if self.backgroud_image_aux.rect[0] <= 0:
+            self.backgroud_image_aux.rect[0] = 360
+    
+    def move_groud(self):
+        self.ground.rect[0] -= 2
+        self.ground_aux.rect[0] -= 2
+
+        if self.ground.rect[0] <= -360:
+            self.ground.rect[0] = 0
+        if self.ground_aux.rect[0] <= 0:
+            self.ground_aux.rect[0] = 360
+
     def gameover(self):
         pass
